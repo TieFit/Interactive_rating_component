168831385a2efc90051ecff1740404c01da94660
TieFit <jonathonnewell123@gmail.com>
2026-03-18 19:35:23 -0400

    Finished JS functionality.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,22 @@ let selectedRating = null;
 
 const ratings = document.querySelector('.ratings');
 const ratingButtons = ratings.querySelectorAll('.rating');
-const submitButton = submitButton.querySelector('.submitButton');
+const submitButton = document.querySelector('.submitButton');
+const ratingState = document.querySelector('.rating_state');
+const thankYouState = document.querySelector('.thank_you_state');
 
 ratingButtons.forEach(function(ratingButtons) {
-    ratingButtons.addEventListener("click", function() {
+    ratingButtons.addEventListener("click", function(event) {
         selectedRating = event.target.getAttribute('data-rating');
-        //console.log(selectedRating);
     });
 });
 
-while (selectedRating == null) {
-
-}
+submitButton.addEventListener("click", function() {
+    if (!selectedRating) {
+        alert('Please select a rating before submitting.');
+        return;
+    }
+    ratingState.classList.add('hidden');
+    thankYouState.classList.remove('hidden');
+    selectedRatingThanks.textContent = `You selected ${selectedRating} out of 5`;
+});
