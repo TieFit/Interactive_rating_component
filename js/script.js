188cd1420942eaa24a2984197d521173874ca879
TieFit <jonathonnewell123@gmail.com>
2026-03-18 17:53:00 -0400

    Added functionality for selecting a rating, and storing it as a variable

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 let selectedRating = null;
 
 const ratings = document.querySelector('.ratings');
-const buttons = ratings.querySelectorAll('.rating');
+const ratingButtons = ratings.querySelectorAll('.rating');
+const submitButton = submitButton.querySelector('.submitButton');
 
-buttons.forEach(function(buttons) {
-    buttons.addEventListener("click", function() {
-        console.log("clicked");
+ratingButtons.forEach(function(ratingButtons) {
+    ratingButtons.addEventListener("click", function() {
+        selectedRating = event.target.getAttribute('data-rating');
+        //console.log(selectedRating);
     });
 });
+
+while (selectedRating == null) {
+
+}
